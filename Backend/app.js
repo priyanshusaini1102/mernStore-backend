7536af6cd6521c9183ae9ab1d686c5549166cd80
priyanshu1102 <priyanshusainidev@gmail.com>
2021-12-22 19:39:46 +0530

    initial commit - 22/12/2021

---
+++ Backend/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+//Routes Imports
+const product = require("./routes/productRoute");
+
+
+module.exports = app;
---
+++ Backend/routes/productRoute.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const { getAllProducts } = require("../controllers/productController.js");
+
+const router = express.Router();
+
+router.route("/products").get(getAllProducts);
+
+module.exports = router;
