a059532cbfc526b2b6625490426225d7ffa009f4
priyanshu1102 <priyanshusainidev@gmail.com>
2022-01-14 22:40:33 +0530

    complete user api and product api

--- Backend/middleWare/error.js
+++ Backend/middleWare/error.js
@@ -9,6 +9,24 @@ module.exports = (err,req,res,next) => {
         err = new ErrorHandler(message,400);
     }
 
+    //mongoose duplicate key error
+    if(err.code == 11000){
+        const message = `Duplicate ${Object.keys(err.keyValue)} entered.`;
+        err = new ErrorHandler(message,400);
+    }
+
+    //wrong jwt error
+    if(err.name === 'JosnWebTokenError'){
+        const message = `Json Web Token is invalid, try again!`;
+        err = new ErrorHandler(message,400);
+    }
+
+    //jwt expire error
+    if(err.name === 'TokenExpireError'){
+        const message = `Json Web Token is Expired, try again!`;
+        err = new ErrorHandler(message,400);
+    }
+
     res.status(err.statusCode).json({
         success: false,
         message: err.message
--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,5 +1,20 @@
-const express = require('express');
-const {registerUser, loginUser, logout, forgotPassword, resetPassword} = require("../controllers/userController");
+const express = require("express");
+const {
+  registerUser,
+  loginUser,
+  logout,
+  forgotPassword,
+  resetPassword,
+  getUserDetails,
+  updatePassword,
+  updateProfile,
+  getAllUsers,
+  getSingleUser,
+  updateUserRole,
+  deleteUser,
+} = require("../controllers/userController");
+const { isAuthenticatedUser, authorizeRoles } = require("../middleWare/auth");
+
 const router = express.Router();
 
 router.route("/register").post(registerUser);
@@ -10,6 +25,18 @@ router.route("/password/forgot").post(forgotPassword);
 
 router.route("/password/reset/:token").put(resetPassword);
 
+router.route("/me").get(isAuthenticatedUser, getUserDetails);
+
 router.route("/logout").get(logout);
+
+router.route("/password/update").put(isAuthenticatedUser, updatePassword);
+
+router.route("/me/update").put(isAuthenticatedUser, updateProfile);
+
+router.route("/admin/users").get(isAuthenticatedUser, authorizeRoles("admin"), getAllUsers);
+
+router.route("/admin/user/:id").get(isAuthenticatedUser, authorizeRoles("admin"), getSingleUser)
+                                .put(isAuthenticatedUser,authorizeRoles("admin"),updateUserRole)
+                                .delete(isAuthenticatedUser,authorizeRoles("admin"),deleteUser);
 
 module.exports = router;
