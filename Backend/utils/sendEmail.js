f7458a01a619c40451ee458484422065c1fa623b
priyanshu1102 <priyanshusainidev@gmail.com>
2022-01-14 14:19:29 +0530

    user route reset link err resolved

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -16,6 +16,14 @@ const sendEmail = async(options)=> {
         text: options.message
     };
 
+    transporter.verify(function (error, success) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Server is ready to take our messages");
+        }
+    });
+
     const info = await transporter.sendMail(mailOptions);
 
 }
